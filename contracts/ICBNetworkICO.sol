a766802b2724566fb3eaa135c5082152ff54d613
adityak_singh <adityak.singh@suffescom.com>
2024-01-08 20:18:23 +0530

    - added the private sale contract

--- contracts/ICBNetworkICO.sol
+++ contracts/ICBNetworkICO.sol
@@ -1,17 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import './ICBNetworkPrivateSale.sol';
 import './ICBNetworkPublicSale.sol';
 import './ICBNetworkPreSale.sol';
 contract ICBNetworkICO{
-    ICBNetworkPrivateSale _privateSale;
+    ICBNetworkPrivateSale public _privateSale;
     ICBNetworkPreSale _one_presale;
     ICBNetworkPreSale _two_presale;
     ICBNetworkPublicSale _public_sale;
 
-
-
-    constructor(){
+    constructor(address privateSaleContractOwner, address funderWallet, address usdtAddress, address usdcAddress){
+        _privateSale = new ICBNetworkPrivateSale(privateSaleContractOwner,funderWallet,usdtAddress, usdcAddress);
       
     }
 
