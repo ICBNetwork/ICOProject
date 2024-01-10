377bd0347d56d00bab0e01deb3bdc7b77d1553a0
adityakrsingh003 <aditya.singh@suffescom.co>
2024-01-10 18:32:22 +0530

    - update the import url of rentrancyGuard

--- contracts/ICB-ICO-Tron.sol
+++ contracts/ICB-ICO-Tron.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract ICB_ICO_TRON is ReentrancyGuard {
     
